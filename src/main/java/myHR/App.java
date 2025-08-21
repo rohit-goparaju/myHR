28607dbce98e526705f0ad13ad20bca8f45ee48f
Rohit Goparaju <rohitgoparaju.r96@gmail.com>
2025-08-21 16:21:41 +0530

    add App.java and log Hello world

---
+++ src/main/java/myHR/App.java
@@ -0,0 +1,14 @@
+package myHR;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+
+	public static final Logger log = LoggerFactory.getLogger("DailyRollingFileLogger");
+	
+	public static void main(String[] args) {
+		log.info("Hello world");
+	}
+	
+}	
