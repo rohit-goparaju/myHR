7d6a1863789ffe2fa10be4ede3eb586aafceca9c
Rohit Goparaju <rohitgoparaju.r96@gmail.com>
2025-08-22 15:10:32 +0530

    add LoginServlet sample class and update index.html

---
+++ src/main/java/servlets/LoginServlet.java
@@ -0,0 +1,22 @@
+package servlets;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().println("Logged in");
+	}
+
+}
