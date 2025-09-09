e5f035957a4e7454afc104ca515747ed72bbb10e
Rohit Goparaju <rohitgoparaju.r96@gmail.com>
2025-09-09 20:11:57 +0530

    initial commit new project

---
+++ src/main/java/com/projects/myHR/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.projects.myHR.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+	@GetMapping("/")
+	public String test() {
+		return "Welcome to myHR project.";
+	}
+}
---
+++ src/test/java/com/projects/myHR/MyHrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.myHR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
